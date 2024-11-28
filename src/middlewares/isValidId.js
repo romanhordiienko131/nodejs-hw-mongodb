995794f8dbe992ea6f8eed5c9507b6b60a879295
Roman Hordiienko <romanhordiienko@proton.me>
2024-11-28 15:17:23 +0200

    validation

---
+++ src/middlewares/isValidId.js
@@ -0,0 +1,9 @@
+import { isValidObjectId } from 'mongoose';
+import createHttpError from 'http-errors';
+
+export const isValidId = (req, res, next) => {
+  const contactId = req.params.contactId;
+  if (!isValidObjectId(contactId)) throw createHttpError(400, 'Bad request');
+
+  next();
+};
