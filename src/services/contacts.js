e2cd542716474c5e86834d7c8f286956a92df2f8
Roman Hordiienko <romanhordiienko@proton.me>
2024-11-29 16:58:07 +0200

    add sorting

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,7 +1,13 @@
+import { SORT_ORDER } from '../constants/index.js';
 import { contactsCollection } from '../db/models/contact.js';
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllContacts = async ({ page, perPage }) => {
+export const getAllContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortBy = SORT_ORDER.ASC,
+  sortOrder = '_id',
+}) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
@@ -11,7 +17,11 @@ export const getAllContacts = async ({ page, perPage }) => {
     .merge(contactsQuery)
     .countDocuments();
 
-  const contacts = await contactsQuery.skip(skip).limit(limit).exec();
+  const contacts = await contactsQuery
+    .skip(skip)
+    .limit(limit)
+    .sort({ [sortBy]: sortOrder })
+    .exec();
 
   const paginationData = calculatePaginationData(contactsCount, page, perPage);
 
---
+++ src/utils/parseSortParams.js
@@ -0,0 +1,35 @@
+import { SORT_ORDER } from '../constants/index.js';
+
+const parseSortBy = (sortBy) => {
+  const keysOfContacts = [
+    '_id',
+    'name',
+    'phoneNumber',
+    'email',
+    'isFavourite',
+    'contactType',
+    'createdAt',
+    'updatedAt',
+  ];
+
+  if (keysOfContacts.includes(sortBy)) return sortBy;
+  return '_id';
+};
+
+const parseSortOrder = (sortOrder) => {
+  const isKnownOrder = [SORT_ORDER.ASC, SORT_ORDER.DESC].includes(sortOrder);
+  if (isKnownOrder) return sortOrder;
+  return SORT_ORDER.ASC;
+};
+
+export const parseSortParams = (query) => {
+  const { sortBy, sortOrder } = query;
+
+  const parsedSortBy = parseSortBy(sortBy);
+  const parsedSortOrder = parseSortOrder(sortOrder);
+
+  return {
+    sortBy: parsedSortBy,
+    sortOrder: parsedSortOrder,
+  };
+};
